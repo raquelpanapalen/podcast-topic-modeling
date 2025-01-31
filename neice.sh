26b98b8f69a36d5540ec4eca278511e49b67633e
Panadero Palenzuela, Raquel <raquel.palenzuela@tuwien.ac.at>
2025-01-31 10:36:08 +0100

    final results

--- neice.sh
+++ neice.sh
@@ -1,8 +1,26 @@
 ROOT_DIR=$1
-DATASET=${ROOT_DIR}/dataset/deezer_podcast_dataset.tsv
-OUTPUT_DIR=${ROOT_DIR}/output
+DATASET=$2
+
+echo ROOT_DIR: $ROOT_DIR
+echo DATASET: $DATASET
+
+if [ "$DATASET" == "deezer" ]; then
+    DATASET_DIR="${ROOT_DIR}/dataset/deezer_podcast_dataset.tsv"
+elif [ "$DATASET" == "itunes" ]; then
+    DATASET_DIR="${ROOT_DIR}/dataset/itunes_popular_podcasts.csv"
+    PREPROCESSED_FILE="${ROOT_DIR}/dataset/itunes_preprocessed.tsv"
+    if [ ! -f "$PREPROCESSED_FILE" ]; then
+        echo "Preprocessing iTunes dataset..."
+        cd ${ROOT_DIR}/ptm/data_preprocessing
+        python preprocess_itunes.py --input_file $DATASET_DIR --output_file $PREPROCESSED_FILE
+    fi
+    DATASET_DIR=$PREPROCESSED_FILE
+fi
+
+PRETRAINED_MODEL_DIR=${ROOT_DIR}/output
+OUTPUT_DIR=${ROOT_DIR}/output/${DATASET}
 MODEL_DIR=${ROOT_DIR}/models
-RESULTS_DIR=${ROOT_DIR}/results
+RESULTS_DIR=${ROOT_DIR}/results/${DATASET}
 BATCH_SIZE=64
 T=10
 k_nearest=500
@@ -23,11 +41,11 @@ fi
 cd ${ROOT_DIR}/ptm/entity_linking
 
 # Download the required files (pre-trained Entity Linker models) if they do not exist
-if [ -d "${OUTPUT_DIR}/wiki_2019" ] && [ -d "${OUTPUT_DIR}/generic" ] && [ -d "${OUTPUT_DIR}/ed-wiki-2019" ]; then
+if [ -d "${PRETRAINED_MODEL_DIR}/wiki_2019" ] && [ -d "${PRETRAINED_MODEL_DIR}/generic" ] && [ -d "${PRETRAINED_MODEL_DIR}/ed-wiki-2019" ]; then
     echo "All required directories exist. No download needed."
 else
     echo "One or more directories missing. Running download script..."
-    ./download-files.sh $OUTPUT_DIR
+    ./download-files.sh $PRETRAINED_MODEL_DIR
 fi
 
 # Generate a JSON file which contains all the NEs extracted and linked from the podcast metadata
@@ -35,35 +53,19 @@ if [ -f "${OUTPUT_DIR}/linked_entities.json" ]; then
     echo "File already exists at ${OUTPUT_DIR}/linked_entities.json"
 else
     echo "Running entity linking script..."
-    ./launch-rel-batch.sh $OUTPUT_DIR $DATASET $OUTPUT_DIR $BATCH_SIZE
+    ./launch-rel-batch.sh $OUTPUT_DIR $DATASET_DIR $PRETRAINED_MODEL_DIR $BATCH_SIZE
 fi
 
 cd ${ROOT_DIR}/ptm/data_preprocessing
 if [ -f "${OUTPUT_DIR}/prepro.txt" ]; then
     echo "File already exists at ${OUTPUT_DIR}/prepro.txt"
 else
-    python main_prepro.py --examples_file $DATASET \
+    python main_prepro.py --examples_file $DATASET_DIR \
     --annotated_file ${OUTPUT_DIR}/linked_entities.json \
     --embeddings_file_path ${MODEL_DIR}/enwiki_20180420_300d.pkl \
     --path_to_save_results ${OUTPUT_DIR}
+
 fi
-
-# Obtain T most similar words to the entities for each alpha_ent in [0.3, 0.4]
-for alpha_ent in 0.30 0.40; do
-
-    if [ -f "${OUTPUT_DIR}/prepro_enrich_entities_th${alpha_ent}_k${k_nearest}.txt" ]; then
-        echo "File already exists at ${OUTPUT_DIR}/prepro_enrich_entities_th${alpha_ent}_k${k_nearest}.txt"
-    else
-        python main_enrich_corpus_using_entities.py --prepro_file ${OUTPUT_DIR}/prepro.txt \
-            --prepro_le_file ${OUTPUT_DIR}/prepro_le.txt \
-            --vocab_file ${OUTPUT_DIR}/vocab.txt \
-            --vocab_le_file ${OUTPUT_DIR}/vocab_le.txt \
-            --embeddings_file_path ${MODEL_DIR}/enwiki_20180420_300d.pkl \
-            --path_to_save_results $OUTPUT_DIR \
-            --alpha_ent ${alpha_ent} \
-            --k ${k_nearest} # ${T}
-    fi
-done
 
 # Download Palmetto and wikipedia_bd for evaluation
 cd $ROOT_DIR/ptm/evaluation
@@ -74,17 +76,17 @@ else
     wget --no-check-certificate https://hobbitdata.informatik.uni-leipzig.de/homes/mroeder/palmetto/palmetto-0.1.0-jar-with-dependencies.jar -P $ROOT_DIR/ptm/evaluation
 fi
 
-if [ -d "${OUTPUT_DIR}/wikipedia_bd" ]; then
-    echo "Directory already exists at ${OUTPUT_DIR}/wikipedia_bd"
+if [ -d "${PRETRAINED_MODEL_DIR}/wikipedia_bd" ]; then
+    echo "Directory already exists at ${PRETRAINED_MODEL_DIR}/wikipedia_bd"
 else
-    wget --no-check-certificate https://hobbitdata.informatik.uni-leipzig.de/homes/mroeder/palmetto/Wikipedia_bd.zip -P $OUTPUT_DIR
-    unzip $OUTPUT_DIR/Wikipedia_bd.zip -d $OUTPUT_DIR
-    rm -rf $OUTPUT_DIR/Wikipedia_bd.zip
+    wget --no-check-certificate https://hobbitdata.informatik.uni-leipzig.de/homes/mroeder/palmetto/Wikipedia_bd.zip -P $PRETRAINED_MODEL_DIR
+    unzip $PRETRAINED_MODEL_DIR/Wikipedia_bd.zip -d $PRETRAINED_MODEL_DIR
+    rm -rf $PRETRAINED_MODEL_DIR/Wikipedia_bd.zip
 fi
 
-WIKIPEDIA_DB_PATH=${OUTPUT_DIR}/wikipedia_bd
+WIKIPEDIA_DB_PATH=${PRETRAINED_MODEL_DIR}/wikipedia_bd
 
-echo "run,K,alpha_word,alpha_ent,evaluation_result" > ${RESULTS_DIR}/results_summary.txt
+echo "run,K,alpha_word,alpha_ent,evaluation_result" > ${RESULTS_DIR}/results_summary.csv
 
 # Extract K topics from the podcast metadata corpus and for each topic its T tops words 
 # Do experiments for K in [20, 50, 100, 200] and alpha_word in [0.2, 0.3, 0.4, 0.5]
@@ -95,7 +97,20 @@ for i in $(seq 1 $RUNS); do
         for alpha_word in 0.2 0.3 0.4 0.5; do
             for alpha_ent in 0.30 0.40; do
 
+                echo "Run: ${i}, K: ${K}, alpha_word: ${alpha_word}, alpha_ent: ${alpha_ent}"
                 cd $ROOT_DIR
+                # Obtain k_nearest most similar words to the entities for each alpha_ent in [0.3, 0.4]
+                python ptm/data_preprocessing/main_enrich_corpus_using_entities.py --prepro_file ${OUTPUT_DIR}/prepro.txt \
+                    --prepro_le_file ${OUTPUT_DIR}/prepro_le.txt \
+                    --vocab_file ${OUTPUT_DIR}/vocab.txt \
+                    --vocab_le_file ${OUTPUT_DIR}/vocab_le.txt \
+                    --embeddings_file_path ${MODEL_DIR}/enwiki_20180420_300d.pkl \
+                    --path_to_save_results $OUTPUT_DIR \
+                    --alpha_ent ${alpha_ent} \
+                    --k ${k_nearest}
+
+                
+                # Run the NEICE model
                 python ptm/neice_model/main.py \
                     --corpus ${OUTPUT_DIR}/prepro_enrich_entities_th${alpha_ent}_k${k_nearest}.txt \
                     --embeddings ${MODEL_DIR}/enwiki_20180420_300d.pkl \
@@ -108,10 +123,13 @@ for i in $(seq 1 $RUNS); do
                     --NED
 
                 cd $ROOT_DIR/ptm/evaluation
-
+                # Evaluate the topics
                 ./evaluate-topics.sh ${OUTPUT_DIR}/top_words.txt ${T} $WIKIPEDIA_DB_PATH > ${OUTPUT_DIR}/evaluation_result.txt
-                EVALUATION_RESULT=$(cat ${OUTPUT_DIR}/evaluation_result.txt)
-                echo "${i},${K},${alpha_word},${alpha_ent},${EVALUATION_RESULT}" >> ${RESULTS_DIR}/results_summary.txt
+                
+                # Round the evaluation result to 4 decimal places
+                EVALUATION_RESULT=$(cat ${OUTPUT_DIR}/evaluation_result.txt | LC_NUMERIC=C awk '{printf "%.4f", $1}')
+                
+                echo "${i},${K},${alpha_word},${alpha_ent},${EVALUATION_RESULT}" >> ${RESULTS_DIR}/results_summary.csv
 
             done
         done
--- tstatistics.py
+++ tstatistics.py
@@ -4,50 +4,100 @@ from scipy.stats import t, ttest_1samp
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+dataset = "itunes"
+
+RESULTS_PATH = f"results/{dataset}"
+
 # Load your results
-my_results = pd.read_csv("results/results_summary.csv")
+my_results = pd.read_csv(f"{RESULTS_PATH}/results_summary.csv")
+
+print(my_results)
 
 # Data from the paper
 paper_results = {
-    20: {
-        (0.2, 0.3): 50.2,
-        (0.2, 0.4): 53.1,
-        (0.3, 0.3): 48.5,
-        (0.3, 0.4): 53.3,
-        (0.4, 0.3): 53.2,
-        (0.4, 0.4): 56.4,
-        (0.5, 0.3): 52.5,
-        (0.5, 0.4): 56.3,
+    "deezer": {
+        20: {
+            (0.2, 0.3): 50.2,
+            (0.2, 0.4): 53.1,
+            (0.3, 0.3): 48.5,
+            (0.3, 0.4): 53.3,
+            (0.4, 0.3): 53.2,
+            (0.4, 0.4): 56.4,
+            (0.5, 0.3): 52.5,
+            (0.5, 0.4): 56.3,
+        },
+        50: {
+            (0.2, 0.3): 48.9,
+            (0.2, 0.4): 49.2,
+            (0.3, 0.3): 52.1,
+            (0.3, 0.4): 50.9,
+            (0.4, 0.3): 51.5,
+            (0.4, 0.4): 52.6,
+            (0.5, 0.3): 56.3,
+            (0.5, 0.4): 60.6,
+        },
+        100: {
+            (0.2, 0.3): 51.4,
+            (0.2, 0.4): 50.8,
+            (0.3, 0.3): 51.5,
+            (0.3, 0.4): 55.3,
+            (0.4, 0.3): 52.2,
+            (0.4, 0.4): 48.1,
+            (0.5, 0.3): 50.8,
+            (0.5, 0.4): 54.9,
+        },
+        200: {
+            (0.2, 0.3): 48.4,
+            (0.2, 0.4): 50.6,
+            (0.3, 0.3): 49.8,
+            (0.3, 0.4): 51.6,
+            (0.4, 0.3): 50.0,
+            (0.4, 0.4): 49.0,
+            (0.5, 0.3): 55.4,
+            (0.5, 0.4): 53.3,
+        },
     },
-    50: {
-        (0.2, 0.3): 48.9,
-        (0.2, 0.4): 49.2,
-        (0.3, 0.3): 52.1,
-        (0.3, 0.4): 50.9,
-        (0.4, 0.3): 51.5,
-        (0.4, 0.4): 52.6,
-        (0.5, 0.3): 56.3,
-        (0.5, 0.4): 60.6,
-    },
-    100: {
-        (0.2, 0.3): 51.4,
-        (0.2, 0.4): 50.8,
-        (0.3, 0.3): 51.5,
-        (0.3, 0.4): 55.3,
-        (0.4, 0.3): 52.2,
-        (0.4, 0.4): 48.1,
-        (0.5, 0.3): 50.8,
-        (0.5, 0.4): 54.9,
-    },
-    200: {
-        (0.2, 0.3): 48.4,
-        (0.2, 0.4): 50.6,
-        (0.3, 0.3): 49.8,
-        (0.3, 0.4): 51.6,
-        (0.4, 0.3): 50.0,
-        (0.4, 0.4): 49.0,
-        (0.5, 0.3): 55.4,
-        (0.5, 0.4): 53.3,
+    "itunes": {
+        20: {
+            (0.2, 0.3): 49.3,
+            (0.2, 0.4): 47.2,
+            (0.3, 0.3): 50.3,
+            (0.3, 0.4): 52.5,
+            (0.4, 0.3): 52.8,
+            (0.4, 0.4): 52.4,
+            (0.5, 0.3): 50.6,
+            (0.5, 0.4): 50.5,
+        },
+        50: {
+            (0.2, 0.3): 43.3,
+            (0.2, 0.4): 49.5,
+            (0.3, 0.3): 52.5,
+            (0.3, 0.4): 49.5,
+            (0.4, 0.3): 50.1,
+            (0.4, 0.4): 51.9,
+            (0.5, 0.3): 46.5,
+            (0.5, 0.4): 52.0,
+        },
+        100: {
+            (0.2, 0.3): 49.5,
+            (0.2, 0.4): 50.7,
+            (0.3, 0.3): 49.0,
+            (0.3, 0.4): 49.2,
+            (0.4, 0.3): 50.6,
+            (0.4, 0.4): 49.9,
+            (0.5, 0.3): 46.7,
+            (0.5, 0.4): 48.7,
+        },
+        200: {
+            (0.2, 0.3): 47.0,
+            (0.2, 0.4): 51.3,
+            (0.3, 0.3): 48.2,
+            (0.3, 0.4): 49.8,
+            (0.4, 0.3): 51.1,
+            (0.4, 0.4): 47.4,
+            (0.5, 0.3): 49.0,
+            (0.5, 0.4): 46.1,
+        },
     },
 }
 
@@ -69,10 +119,12 @@ grouped = my_results.groupby(["K", "alpha_word", "alpha_ent"])["evaluation_resul
 for (k, alpha_word, alpha_ent), group in grouped:
     # Mean and confidence intervals for your results
     mean, ci = confidence_interval(group)
-    variance = np.var(group, ddof=1)
+    variance = np.var(group)
+
+    print(group, variance)
 
     # Get the corresponding value from the paper
-    paper_result = paper_results.get(k, {}).get((alpha_word, alpha_ent))
+    paper_result = paper_results[dataset][k].get((alpha_word, alpha_ent))
 
     if paper_result is not None:
         # Perform t-test to check if your results differ from the paper's
@@ -97,11 +149,10 @@ for (k, alpha_word, alpha_ent), group in grouped:
 comparison_df = pd.DataFrame(comparison_results)
 
 # Save comparison results to a CSV file
-comparison_df.to_csv("results/comparison_results.csv", index=False)
+comparison_df.to_csv(f"{RESULTS_PATH}/comparison_results.csv", index=False)
 
 # Print results
 print(comparison_df)
-
 
 # Bar Plot: Mean vs Paper Result
 hues = ["alpha_word", "alpha_ent"]
@@ -115,7 +166,7 @@ for hue in hues:
         plt.ylabel(y_var)
         plt.xlabel("K")
         plt.legend(title=hue)
-        plt.savefig(f"results/{y_var}_K_{hue}.png")
+        plt.savefig(f"{RESULTS_PATH}/{y_var}_K_{hue}.png")
 
 
 # Heatmap: Mean-Paper Differences
@@ -131,9 +182,50 @@ for i, k in enumerate(comparison_df["K"].unique()):
     )
 
     subplot = sns.heatmap(
-        heatmap_data, annot=True, cmap="coolwarm", center=0, ax=axes[i // 2, i % 2]
+        heatmap_data,
+        annot=True,
+        cmap="coolwarm",
+        center=0,
+        ax=axes[i // 2, i % 2],
+        annot_kws={"fontsize": 15},
     )
-    subplot.set_title(f"Mean-Paper Result Differences for K={k}")
+    subplot.collections[0].colorbar.ax.tick_params(labelsize=15)
+    # subplot.collections[0].colorbar.ax.set_ylabel("Mean Difference", fontsize=15)
+    subplot.set_title(f"Mean Difference for K={k}", fontsize=20)
+    subplot.set_xlabel("alpha_ent", fontsize=15)
+    subplot.set_ylabel("alpha_word", fontsize=15)
+    subplot.set_xticklabels(subplot.get_xticklabels(), fontsize=15)
+    subplot.set_yticklabels(subplot.get_yticklabels(), fontsize=15)
+
 
 plt.tight_layout()
-plt.savefig("results/mean_paper_diff_heatmap.png")
+plt.savefig(f"{RESULTS_PATH}/mean_paper_diff_heatmap.png")
+
+
+# Heatmap: P-Values
+fig, axes = plt.subplots(2, 2, figsize=(12, 10))
+
+# for each K value, plot a heatmap
+for i, k in enumerate(comparison_df["K"].unique()):
+    heatmap_data = comparison_df[comparison_df["K"] == k].pivot(
+        index="alpha_word", columns="alpha_ent", values="p_value"
+    )
+
+    subplot = sns.heatmap(
+        heatmap_data,
+        annot=True,
+        cmap="coolwarm",
+        # cbar_kws={"label": "p-value"},
+        annot_kws={"fontsize": 15},
+        ax=axes[i // 2, i % 2],
+    )
+    subplot.collections[0].colorbar.ax.tick_params(labelsize=15)
+    # subplot.collections[0].colorbar.ax.set_ylabel("p-value", fontsize=15)
+    subplot.set_xticklabels(subplot.get_xticklabels(), fontsize=15)
+    subplot.set_yticklabels(subplot.get_yticklabels(), fontsize=15)
+    subplot.set_title(f"P-Values for K={k}", fontsize=20)
+    subplot.set_xlabel("alpha_ent", fontsize=15)
+    subplot.set_ylabel("alpha_word", fontsize=15)
+
+plt.tight_layout()
+plt.savefig(f"{RESULTS_PATH}/p_value_heatmap.png")
